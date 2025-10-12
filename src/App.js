29adcb75f2802c7cf1292c4813aece13b68a39cf
Vidhi Bajaj <vidhibajaj1401@gmail.com>
2025-10-12 13:11:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/AiEvoked">
       <div className="App">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* Add more routes as needed */}
-          {/* <Route path="/product" element={<div>Product Page</div>} />
-          <Route path="/resources" element={<div>Resources Page</div>} />
-          <Route path="/documentation" element={<div>Documentation Page</div>} />
-          <Route path="/company" element={<div>Company Page</div>} />
-          <Route path="/pricing" element={<div>Pricing Page</div>} />
-          <Route path="/login" element={<div>Login Page</div>} /> */}
         </Routes>
       </div>
     </Router>
